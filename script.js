05507fbdc4fe9e4ea92e25cdad96687b2971f179
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-02-07 12:37:36 +0100

    Informacja o poprawnym dodaniu do bazy

--- script.js
+++ script.js
@@ -19,16 +19,17 @@ const errorDescription = document.querySelector(".error-description");
 const tagsInput = document.querySelector(".tags");
 const errorTags = document.querySelector(".error-tags");
 const addFormInputs = document.querySelectorAll(".add-form input");
+const isSubmittedMessage = document.querySelector(".submitted-message-box");
 
 addBtn.disabled = true;
 
 // Warunkowe odblokowanie przycisku dodaj
 const checkReady = () => {
-  addFormInputs.forEach(input => {
+  addFormInputs.forEach((input) => {
     let errorMessage = input.nextElementSibling;
     addBtn.disabled = input.value.length > 0 && errorMessage.style.display === "none" ? false : true;
-  })
-}
+  });
+};
 // puste pola
 addFormInputs.forEach((input) => {
   input.addEventListener("blur", () => {
@@ -36,23 +37,26 @@ addFormInputs.forEach((input) => {
     if (input.value.length === 0) {
       errorMessage.textContent = "Pole nie może być puste!";
       errorMessage.style.display = "block";
-    } 
-    else if (input.className.includes("date") && /[a-z]/ig.test(dateInput.value)) {
+    } else if (input.className.includes("date") && /[a-z]/gi.test(dateInput.value)) {
       errorMessage.textContent = "Pole może zawierać tylko cyfry!";
       errorMessage.style.display = "block";
-    }
-    else if (input.className.includes("date") && (input.value.length !== 10 || input.value.match(/\./ig).length !== 2)) {
-      errorMessage.textContent = "Pole musi być w formacie \"01.01.2023\"";
-      errorMessage.style.fontSize = "13px"
+    } else if (input.className.includes("date") && (input.value.length !== 10 || input.value.match(/\./gi).length !== 2)) {
+      errorMessage.textContent = 'Pole musi być w formacie "01.01.2023"';
+      errorMessage.style.fontSize = "13px";
       errorMessage.style.display = "block";
-    }
-    else {
+    } else {
       errorMessage.style.display = "none";
       checkReady();
     }
   });
 });
 
+addBtn.addEventListener("click", () => {
+  isSubmittedMessage.style.opacity = "1";
+  setTimeout(() => {
+    isSubmittedMessage.style.opacity = "0";
+  }, 10000)
+});
 
 // RENDEROWANIE LISTY Z FILES
 // dynamiczne dodawanie klas i tekstu do buttonów
